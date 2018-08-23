cadc9e5a91e02fdfe9b092cd8fc862746d6da30e
Treize Interactif <pierre@13i.fr>
2018-08-24 00:56:48 +0200

    Save metabox

--- views/metabox.php
+++ views/metabox.php
@@ -4,7 +4,7 @@
         <?php _e('Title', 'w6-wp-seo'); ?>
     </h3>
     <p>
-        <input type="text" name="title" value="<?php echo $title; ?>" />
+        <input type="text" name="w6wpseo_title" value="<?php echo $title; ?>" />
     </p>
 </div>
 <!-- End : W6 Wp Seo Metabox -->
